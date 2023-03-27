57f42dbce77917ec0e588771a5579a39bfc35646
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-27 23:18:52 +0100

    create many subscribers, abstract subscriberSchema

--- apps/web/src/server/api/routers/subscribers.ts
+++ apps/web/src/server/api/routers/subscribers.ts
@@ -2,26 +2,31 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const subscriberSchema = z.object({
+  email: z.string().email(),
+  name: z.string(),
+  subscribed: z.boolean(),
+  createdAt: z.date().optional(),
+  ListId: z.number().int(),
+});
+
 export const subscriberRouter = createTRPCRouter({
-  create: publicProcedure
-    .input(
-      z.object({
-        email: z.string().email(),
-        name: z.string(),
-        subscribed: z.boolean(),
-        createdAt: z.date().optional(),
-        ListId: z.number().int(),
-      })
-    )
+  create: publicProcedure.input(subscriberSchema).mutation(({ input, ctx }) => {
+    return ctx.prisma.subscriber.create({
+      data: {
+        email: input.email,
+        subscribed: input.subscribed,
+        name: input.name,
+        createdAt: input.createdAt,
+        ListId: input.ListId,
+      },
+    });
+  }),
+  createMultiple: publicProcedure
+    .input(z.array(subscriberSchema).nonempty())
     .mutation(({ input, ctx }) => {
-      return ctx.prisma.subscriber.create({
-        data: {
-          email: input.email,
-          subscribed: input.subscribed,
-          name: input.name,
-          createdAt: input.createdAt,
-          ListId: input.ListId,
-        },
+      return ctx.prisma.subscriber.createMany({
+        data: input,
       });
     }),
   getAllFromList: publicProcedure
