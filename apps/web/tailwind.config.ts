a8ca4d2ff7d154cec8e23b3653c76cb751d4e4b8
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-28 22:00:15 +0100

    define tailwind config in ts instead of cjs

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,26 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-// @ts-ignore
-const { slateDarkA: slate, mintDarkA: mint } = require("@radix-ui/colors");
-// @ts-ignore
-const onlyNumbers = (str) => str.replace(/[^0-9]/g, "");
+import type { Config } from "tailwindcss";
+
+import { slateDarkA as slate, mintDarkA as mint } from "@radix-ui/colors";
+
+const onlyNumbers = (str: string) => str.replace(/[^0-9]/g, "");
 
 // By default radix colors would create classes like `bg-slate-slateA1` or `bg-slateA1`. passing a color object trough this function creates classes like `bg-slate-1`
-// @ts-ignore
-const numberfyRadixColorNames = (colors) => {
-  const numberfied = {};
+const numberfyRadixColorNames = (colors: { [key: string]: string }) => {
+  const numberfied: {
+    [key: string]: string;
+  } = {};
   for (const [key, value] of Object.entries(colors)) {
     if (typeof value === "string") {
-      // @ts-ignore
       numberfied[onlyNumbers(key)] = value;
-    } else {
-      // @ts-ignore
-      numberfied[key] = numberfyRadixColorNames(value);
     }
   }
   return numberfied;
 };
 
-const config = {
+export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
@@ -34,6 +31,4 @@ const config = {
     extend: {},
   },
   plugins: [],
-};
-
-module.exports = config;
+} satisfies Config;
