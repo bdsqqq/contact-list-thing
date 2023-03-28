07c9ece5ec378ed5341754f202773bfa18789c95
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-28 21:48:20 +0100

    make the color classes look like the ones in resend.com eg: bg-slate-1 instead of bg-slate-slateA1

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -1,6 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 // @ts-ignore
-const { slateDarkA, mintDarkA } = require("@radix-ui/colors");
+const { slateDarkA: slate, mintDarkA: mint } = require("@radix-ui/colors");
+// @ts-ignore
+const onlyNumbers = (str) => str.replace(/[^0-9]/g, "");
+
+// By default radix colors would create classes like `bg-slate-slateA1` or `bg-slateA1`. passing a color object trough this function creates classes like `bg-slate-1`
+// @ts-ignore
+const numberfyRadixColorNames = (colors) => {
+  const numberfied = {};
+  for (const [key, value] of Object.entries(colors)) {
+    if (typeof value === "string") {
+      // @ts-ignore
+      numberfied[onlyNumbers(key)] = value;
+    } else {
+      // @ts-ignore
+      numberfied[key] = numberfyRadixColorNames(value);
+    }
+  }
+  return numberfied;
+};
 
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
@@ -10,12 +28,8 @@ const config = {
       current: "currentColor",
       black: "#000",
       white: "#fff",
-      slate: {
-        ...slateDarkA,
-      },
-      mint: {
-        ...mintDarkA,
-      },
+      slate: numberfyRadixColorNames(slate),
+      mint: numberfyRadixColorNames(mint),
     },
     extend: {},
   },
