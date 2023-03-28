b40d749288531da80984e7d8b1bf05559b835768
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-28 21:39:19 +0100

    use radix colors in tailwind config

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -1,7 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+// @ts-ignore
+const { slateDarkA, mintDarkA } = require("@radix-ui/colors");
+
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      slate: {
+        ...slateDarkA,
+      },
+      mint: {
+        ...mintDarkA,
+      },
+    },
     extend: {},
   },
   plugins: [],
