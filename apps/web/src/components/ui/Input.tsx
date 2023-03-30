079b6691155c2a86294ea43956f27ad87f29b68e
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-30 20:19:49 +0100

    add Input Component

---
+++ apps/web/src/components/ui/Input.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { cn } from "~/utils/styleHelpers";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        className={cn(
+          "border-slate-6 bg-slate-4 text-slate-12 focus:ring-slate-7 placeholder:text-slate-11 relative h-8 w-full select-none appearance-none rounded-md border px-2 pl-[var(--text-field-left-slot-width)] pr-[var(--text-field-right-slot-width)] text-sm outline-none transition duration-200 ease-in-out focus:ring-2",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
