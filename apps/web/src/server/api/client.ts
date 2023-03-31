9f08b731d1595813e549090877e01f5d6defa58c
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-31 21:50:48 +0100

    add todo

--- apps/web/src/server/api/client.ts
+++ apps/web/src/server/api/client.ts
@@ -4,6 +4,7 @@ import superjson from "superjson";
 
 import { env } from "~/env.mjs";
 
+// TODO: thers a NEXT_env_url thingy I could use here I think
 const getUrl = () => {
   if (env.NODE_ENV === "development") {
     return "http://192.168.1.238:3000/api/trpc";
