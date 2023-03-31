284636e682112d684b8dfd7f843dab4fdc96300d
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-31 21:49:46 +0100

    prepare client for prod env

--- apps/web/src/server/api/client.ts
+++ apps/web/src/server/api/client.ts
@@ -2,7 +2,17 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "./root";
 import superjson from "superjson";
 
+import { env } from "~/env.mjs";
+
+const getUrl = () => {
+  if (env.NODE_ENV === "development") {
+    return "http://192.168.1.238:3000/api/trpc";
+  } else {
+    return "http://contact-list-thing-web.vercel.app/api/trpc";
+  }
+};
+
 export const client = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: "http://192.168.1.238:3000/api/trpc" })],
+  links: [httpBatchLink({ url: getUrl() })],
   transformer: superjson,
 });
